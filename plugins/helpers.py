1a2f1559be5b3da87124f1c9e8c5d03b165c6096
telegramchannels <itgoyo@foxmail.com>
2026-03-31 00:13:12 +0800

    add somethings

--- plugins/helpers.py
+++ plugins/helpers.py
@@ -138,5 +138,4 @@ def build_start_text():
         f"`/jx <链接>` - 解析并发送媒体\n"
         f"`/raw <链接>` - 不处理媒体, 发送原始文件\n"
         f"`/zip <链接>` - 不处理媒体, 保存解析结果, 发送压缩包\n\n"
-        f"**开源地址: [GitHub](https://github.com/z-mio/parse_hub_bot)**"
     )
